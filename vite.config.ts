cd60d6112bd14bfae850556dfbd6b21abe9ddc66
ZzRanger <calebmarklim@gmail.com>
2023-05-14 18:38:59 -0500

    vite config update again

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    {
+      ...(await import("@mdx-js/rollup")).default({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx",
+      }),
+      enforce: "pre",
+    },
     solid({
-      ssr: false,
-      adapter: vercel({ edge: true }),
+      extensions: [".mdx", ".md"],
+      adapter: vercel(),
     }),
   ],
 });
