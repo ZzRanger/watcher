0b145bf8d674396214623155fc16698fc861e2ae
ZzRanger <calebmarklim@gmail.com>
2023-05-15 19:01:38 -0500

    update qrcode

--- src/routes/session/[code]/qrcode.tsx
+++ src/routes/session/[code]/qrcode.tsx
@@ -1,5 +1,5 @@
 import { QRCodeSVG } from "solid-qr-code";
-import { useParams } from "solid-start";
+import { A, useParams } from "solid-start";
 
 const baseUrl = import.meta.env.VITE_VERCEL_URL;
 
@@ -13,10 +13,39 @@ export default function QRCode() {
         "flex-direction": "column",
         "align-items": "center",
         padding: "16px",
+        "justify-content": "center",
+        height: "100vh",
       }}
     >
+      <div
+        style={{
+          width: "24px",
+          height: "24px",
+          position: "absolute",
+          top: "20px",
+          left: "20px",
+        }}
+      >
+        <A href={`/session/${params.code}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+            color="black"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </A>
+      </div>
       <QRCodeSVG
-        value={`${baseUrl}/session/${params.code}`}
+        value={`https://${baseUrl}/session/${params.code}`}
         size={128}
         bgColor={"#ffffff"}
         fgColor={"#000000"}
